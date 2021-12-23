650a9503c35edac8f3fc5afd1689670b4805f837
xFinal <wentao.xin@gmail.com>
2021-12-23 11:21:54 +0800

    Create tf2_to_mlir.py

---
+++ tf2_to_mlir.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow.lite.python.util import run_graph_optimizations, get_grappler_config
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2_as_graph
+import os
+
+
+def main():
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+    model = tf.keras.models.load_model("./mobilenet_v2_140_224", custom_objects={'KerasLayer': hub.KerasLayer})
+
+    fff = tf.function(model).get_concrete_function(tf.TensorSpec([1, 224, 224, 3], tf.float32))
+    frozen_func, graph_def = convert_variables_to_constants_v2_as_graph(fff)
+
+    input_tensors = [
+        tensor for tensor in frozen_func.inputs
+        if tensor.dtype != tf.resource
+    ]
+    output_tensors = frozen_func.outputs
+
+    graph_def = run_graph_optimizations(
+        graph_def,
+        input_tensors,
+        output_tensors,
+        config=get_grappler_config(['pruning', 'function', 'constfold', 'shape', 'remap', 'memory', 'common_subgraph_elimination', 'arithmetic', 'loop', 'dependency', 'debug_stripper']),
+        graph=frozen_func.graph)
+
+    tf_mlir_graph = tf.mlir.experimental.convert_graph_def(graph_def, pass_pipeline='tf-standard-pipeline')
+    outfile = open('nsfw.mlir', 'wb')
+    outfile.write(tf_mlir_graph.encode())
+    outfile.close()
+
+
+if __name__ == "__main__":
+    main()
